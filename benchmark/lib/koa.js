59f57665117e9af12094689cb3aa6eec18d2b871
intrnl <intrnl28+git@gmail.com>
2021-01-03 06:35:14 +0700

    chore(benchmark): add koa

---
+++ benchmark/lib/koa.js
@@ -0,0 +1,31 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+
+
+
+function one ({ request }, next) {
+	request.one = true;
+	return next();
+}
+
+function two ({ request }, next) {
+	request.two = true;
+	return next();
+}
+
+
+let app = new Koa()
+let router = new Router();
+
+router
+	.get('/favicon.ico', () => {})
+	.get('/', (ctx) => { ctx.body = 'Hello' })
+	.get('/user/:id', (ctx) => { ctx.body = `User: ${ctx.params.id}` });
+
+app
+	.use(one)
+	.use(two)
+	.use(router.routes())
+	.use(router.allowedMethods)
+	.listen(3030);
